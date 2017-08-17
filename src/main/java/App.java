c612775d40352b2a671dd5d905a889ad533dca8d
RxNunez <Nunez.ryan@gmail.com>
2017-08-17 01:08:08 -0700

    include get new walker in app and add walker-form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+import models.Walker;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
+
 public class App {
+
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/walker/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "walker-form.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
 }
