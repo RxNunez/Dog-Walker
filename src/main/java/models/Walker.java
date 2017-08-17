a21b58c12fc5b1226a962af57e45b72617ad4d28
RxNunez <Nunez.ryan@gmail.com>
2017-08-16 21:48:37 -0700

    include class, variables, constructor and test 1

---
+++ src/main/java/models/Walker.java
@@ -0,0 +1,23 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Walker {
+
+    private String walkerName;
+    private static ArrayList<Walker>instances = new ArrayList<>();
+    private List<WalkerDog> walkerDogs;
+    private int id;
+
+    public Walker (String walkerName) {
+        this.walkerName = walkerName;
+        instances.add(this);
+        walkerDogs = new ArrayList<WalkerDog>();
+        this.id = instances.size();
+    }
+
+    public String getWalkerName() {
+        return walkerName;
+    }
+}
---
+++ src/test/java/models/WalkerTest.java
@@ -0,0 +1,27 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WalkerTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void NewWalkerObjectGetsCorrectlyCreates_true() throws Exception {
+        Walker walker = new Walker("Ryan");
+        assertEquals(true, walker instanceof Walker);
+    }
+
+
+
+}
