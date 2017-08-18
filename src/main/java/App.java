2e6ea8d5bc4ba618e4eb69447a160177ad6a4278
RxNunez <Nunez.ryan@gmail.com>
2017-08-18 08:29:03 -0700

    include show all walkers and index.hbs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -27,5 +27,13 @@ public class App {
             return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
 
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            ArrayList<Walker> walker = Walker.getAll();
+            model.put("walker", walker);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }
